1995c9c2f4951a960fb7f630cc5d3285bf4e8e51
jaguar3096 <jaguar3096@gmail.com>
2017-10-31 23:13:14 +0800

    add stochastic GD

--- Gradient_Descent.py
+++ Gradient_Descent.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import random
 
 '''
 A code for gradient descent method.
@@ -26,36 +27,20 @@ def cost_fn(hypo_fn, theta, data_x, data_y):
     temp = 0
 
     for i in range(m):
-        temp += 1/m * ( hypo_fn(theta, data_x[i]) - data_y[i] )**2
+        temp += 1.0/(2.0*m) * ( hypo_fn(theta, data_x[i]) - data_y[i] )**2
 
     return temp
 
 #===============================================================
 # Gradient of cost function
 #===============================================================
-def gradient_cost_fn(hypo_fn, theta, data_xi, data_yi, j):
-    return 2.0/m * ( data_yi - hypo_fn(theta, data_xi) )*data_xi[j]
+def gradient_cost_fn(hypo_fn, theta, data_xi, data_yi, num_m, j ):
+    return 1.0/num_m * ( hypo_fn(theta, data_xi) - data_yi )*data_xi[j]
 
 #===============================================================
-# Updating theta vetor
+# run batch gradient descent
 #===============================================================
-def updating_theta(hypo_fn, alpha, theta, data_x, data_y, j):
-    
-    right_term = 0
-    theta_j = 0
-
-    for i in range(m):
-        right_term += gradient_cost_fn(hypo_fn, theta, data_x[i], data_y[i], j)
-
-    theta_j  = theta[j] + alpha*right_term
-    
-    return theta_j
-
-#===============================================================
-# run gradient descent
-#===============================================================
-def gradient_descent_batch( m, n, theta, alpha, data_x, data_y,
-                            error_tolerance, max_iter ):
+def batch_GD( m, n, theta, alpha, data_x, data_y, error_tolerance, max_iter ):
 
     converged = False
     current_iter = 0
@@ -64,7 +49,15 @@ def gradient_descent_batch( m, n, theta, alpha, data_x, data_y,
     while not converged:
         
         for j in range(n):
-            theta_new[j] = updating_theta( hypo_fn, alpha, theta, data_x, data_y, j )
+
+            right_term = 0
+            num_m = m
+
+            for i in range(m):
+                right_term += -gradient_cost_fn( hypo_fn, theta, data_x[i], data_y[i], num_m, j )
+
+            theta_new[j]  = theta[j] + alpha*right_term
+
 
         theta = theta_new
 
@@ -82,6 +75,49 @@ def gradient_descent_batch( m, n, theta, alpha, data_x, data_y,
         print ("Iterations: {0}, \nValue of cost function: {1}\n".format(current_iter, value))
 
     return theta
+
+#===============================================================
+# run stochastic(or mini batch) gradient descent
+#===============================================================
+def stochastic_GD( m, n, theta, alpha, data_x, data_y, error_tolerance, max_iter, num_batch=1 ):
+
+    converged = False
+    current_iter = 0
+    value_0 = cost_fn(hypo_fn, theta, data_x, data_y)
+    theta_new = np.zeros(n)
+    while not converged:
+        
+        for j in range(n):
+
+            right_term = 0
+            num_m = num_batch
+            ind = []
+            for _ in range(num_m):
+                ind.append( random.randint(0, m - 1) )  #take random integer from 0 ~ m
+
+            for i in range(num_m):
+                right_term += -gradient_cost_fn( hypo_fn, theta, data_x[ind[i]], data_y[ind[i]], num_m, j  )
+
+            theta_new[j]  = theta[j] + alpha*right_term
+
+
+        theta = theta_new
+
+        value = cost_fn(hypo_fn, theta, data_x, data_y)
+
+        if current_iter > max_iter:
+            converged = True
+
+        elif ( abs(value-value_0) <= error_tolerance ):
+            converged = True
+
+        current_iter += 1
+        value_0  = value
+        
+        print ("Iterations: {0}, \nValue of cost function: {1}\n".format(current_iter, value))
+
+    return theta
+
 
 #===============================================================
 # plot result
@@ -123,9 +159,11 @@ if __name__ == '__main__':
     error_tolerance = 10e-5             # tolerance value of error
     max_iter = 10000                    # maximum number of iterations
 
-    theta_final = gradient_descent_batch( m, n, theta, alpha, data_x, data_y,
-                                          error_tolerance, max_iter )
+    # theta_final = batch_GD( m, n, theta, alpha, data_x, data_y,
+    #                         error_tolerance, max_iter )
 
+    theta_final = stochastic_GD( m, n, theta, alpha, data_x, data_y,
+                                 error_tolerance, max_iter, num_batch=5 )
 
     print (theta_final, id(theta))
     print ('gradient descent complete!')
